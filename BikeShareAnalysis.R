a58ec33b15ba0a95bd88836bbf792b559d6debc8
Cole Bowen <bowencolee@gmail.com>
2023-09-18 14:06:41 -0600

    Updated analysis, Poisson reg submission

--- BikeShareAnalysis.R
+++ BikeShareAnalysis.R
@@ -5,6 +5,7 @@
 library(tidyverse)
 library(tidymodels)
 library(vroom)
+library(poissonreg)
 
 bike_train <- vroom::vroom("C:/Users/bowen/Desktop/Stat348/KagglebikeShare/train.csv")
 bike_test <- vroom::vroom("C:/Users/bowen/Desktop/Stat348/KagglebikeShare/test.csv")
@@ -13,29 +14,36 @@ bike_test <- vroom::vroom("C:/Users/bowen/Desktop/Stat348/KagglebikeShare/test.c
 bike_train <- bike_train %>%
   select(-casual,-registered)
 
+################
+### Cleaning ###
+################
 
-# Cleaning section
-bike_clean <- bike_train %>%
-  mutate(weather = ifelse(weather == 4,3,weather)) %>% # get rid of the one level 4 weather var.
-  mutate(season = factor(season, # change the season to factor with names
-                         levels = 1:4,
-                         labels = c("spring", "summer", "fall", "winter"))) %>%
-  select(datetime,season,temp,atemp,humidity,windspeed,count)
+# my_recipe <- recipe(count~., data = bike_train) %>%
+#  ste_mutate(weather = ifelse(weather == 4,3,weather)) %>%
+#  step_time(datetime, features=c("hour", "minute")) %>%
+#  step_dummy(all_nominal_predictors()) %>%
+#  step_zv(all_predictors())
 
-# mutate(weather = ifelse(weather == 4,3,weather))
-
-# Engineering section
-my_recipe <- recipe(count~., data = bike_train) %>%
-  ste_mutate(weather = ifelse(weather == 4,3,weather)) %>%
-  step_time(datetime, features=c("hour", "minute")) %>%
-  step_dummy(all_nominal_predictors()) %>%
-  step_zv(all_predictors())
+# Engineering section ### IF YOU WANT TO DO SOMETHING TO BOTH, DO IT IN RECIPE#
+my_recipe <- recipe(count~., data=bike_train) %>%
+  step_mutate(weather=ifelse(weather==4, 3, weather)) %>% #Relabel weather 4 to 3
+  step_mutate(weather=factor(weather, levels=1:3, labels=c("Sunny", "Mist", "Rain"))) %>%
+  step_mutate(season=factor(season, levels=1:4, labels=c("Spring", "Summer", "Fall", "Winter"))) %>%
+  step_mutate(holiday=factor(holiday, levels=c(0,1), labels=c("No", "Yes"))) %>%
+  step_mutate(workingday=factor(workingday,levels=c(0,1), labels=c("No", "Yes"))) %>%
+  step_time(datetime, features="hour") %>%
+  step_rm(datetime)
   
 # Baking the recipe
 prepped_recipe <- prep(my_recipe)
 bike_bake <- bake(prepped_recipe, bike_train)
 
-#Linear regression
+
+
+
+#########################
+### Linear regression ###
+#########################
 my_mod <- linear_reg() %>% #Type of model
   set_engine("lm") # Engine = What R function to use
 
@@ -48,15 +56,42 @@ bike_predictions <- predict(bike_workflow,
                             new_data=bike_test) # Use fit to predict
 
 # Make the submission csv
-bike_submit <- cbind(bike_test,bike_predictions) %>%
+bike_linear <- cbind(bike_test,bike_predictions) %>%
   select(datetime,.pred)
-colnames(bike_submit) <- c("datetime", "count")
+colnames(bike_linear) <- c("datetime", "count")
 
 # Assuming 'df' is your data frame and 'negative_column' is the column with negative values
-bike_submit$count[bike_submit$count < 0] <- 0
+bike_linear$count[bike_linear$count < 0] <- 0
 
-bike_submit$datetime <- format(bike_submit$datetime, "%Y-%m-%d %H:%M:%S")
+bike_linear$datetime <- format(bike_linear$datetime, "%Y-%m-%d %H:%M:%S")
 
 # Get the csv file
-vroom_write(bike_submit, "bike_submit.csv", ",")
+vroom_write(bike_linear, "bike_linear.csv", ",")
 
+
+
+##########################
+### Poisson Regression ###
+##########################
+
+pois_mod <- poisson_reg() %>% #Type of model
+  set_engine("glm") # GLM = generalized linear model
+
+bike_pois_workflow <- workflow() %>%
+add_recipe(my_recipe) %>%
+add_model(pois_mod) %>%
+fit(data = bike_train) # Fit the workflow
+
+bike_predictions_pois <- predict(bike_pois_workflow,
+                            new_data=bike_test) # Use fit to predict
+
+# Make the submission csv
+bike_pois <- cbind(bike_test,bike_predictions) %>%
+  select(datetime,.pred)
+colnames(bike_pois) <- c("datetime", "count")
+
+
+bike_pois$datetime <- format(bike_pois$datetime, "%Y-%m-%d %H:%M:%S")
+
+# Get the csv file
+vroom_write(bike_pois, "bike_pois.csv", ",")
