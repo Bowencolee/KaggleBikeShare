87ee6e1bda52847e101205e2c450d95c24e73183
Cole Bowen <bowencolee@gmail.com>
2023-09-20 12:32:51 -0600

    Updated with log-linear regression

--- BikeShareAnalysis.R
+++ BikeShareAnalysis.R
@@ -10,19 +10,16 @@ library(poissonreg)
 bike_train <- vroom::vroom("C:/Users/bowen/Desktop/Stat348/KagglebikeShare/train.csv")
 bike_test <- vroom::vroom("C:/Users/bowen/Desktop/Stat348/KagglebikeShare/test.csv")
 
-# Bike_train w/o casual and registered
-bike_train <- bike_train %>%
-  select(-casual,-registered)
-
 ################
 ### Cleaning ###
 ################
 
-# my_recipe <- recipe(count~., data = bike_train) %>%
-#  ste_mutate(weather = ifelse(weather == 4,3,weather)) %>%
-#  step_time(datetime, features=c("hour", "minute")) %>%
-#  step_dummy(all_nominal_predictors()) %>%
-#  step_zv(all_predictors())
+# Bike_train w/o casual and registered (bike_log takes the count and logs it)
+bike_train <- bike_train %>%
+  select(-casual,-registered)
+
+bike_log <- bike_train %>%
+  mutate(count=log(count))
 
 # Engineering section ### IF YOU WANT TO DO SOMETHING TO BOTH, DO IT IN RECIPE#
 my_recipe <- recipe(count~., data=bike_train) %>%
@@ -32,6 +29,8 @@ my_recipe <- recipe(count~., data=bike_train) %>%
   step_mutate(holiday=factor(holiday, levels=c(0,1), labels=c("No", "Yes"))) %>%
   step_mutate(workingday=factor(workingday,levels=c(0,1), labels=c("No", "Yes"))) %>%
   step_time(datetime, features="hour") %>%
+  step_dummy(all_nominal_predictors()) %>%
+  step_zv(all_predictors())
   step_rm(datetime)
   
 # Baking the recipe
@@ -69,6 +68,28 @@ bike_linear$datetime <- format(bike_linear$datetime, "%Y-%m-%d %H:%M:%S")
 vroom_write(bike_linear, "bike_linear.csv", ",")
 
 
+#############################
+### Log-Linear Regression ###
+#############################
+my_mod <- linear_reg() %>% #Type of model
+  set_engine("lm") # Engine = What R function to use
+
+log_bike_workflow <- workflow() %>%
+  add_recipe(my_recipe) %>%
+  add_model(my_mod) %>%
+  fit(data = bike_log) # Fit the workflow
+
+## Get Predictions for test set AND format for Kaggle
+log_lin_preds <- predict(log_bike_workflow, new_data = bike_test) %>% #This predicts log(count)
+  mutate(.pred=exp(.pred)) %>% # Back-transform the log to original scale
+  bind_cols(., bike_test) %>% #Bind predictions with test data
+  select(datetime, .pred) %>% #Just keep datetime and predictions
+  rename(count=.pred) %>% #rename pred to count (for submission to Kaggle)
+  mutate(count=pmax(0, count)) %>% #pointwise max of (0, prediction)
+  mutate(datetime=as.character(format(datetime))) #needed for right format to Kaggle
+## Write predictions to CSV
+vroom_write(x=log_lin_preds, file="./bike_log_linear.csv", delim=",")
+
 
 ##########################
 ### Poisson Regression ###
@@ -93,5 +114,4 @@ colnames(bike_pois) <- c("datetime", "count")
 
 bike_pois$datetime <- format(bike_pois$datetime, "%Y-%m-%d %H:%M:%S")
 
-# Get the csv file
 vroom_write(bike_pois, "bike_pois.csv", ",")
